7d5751bbe280d6fbe29f1b9557a30542ef3b9ba8
suca19 <carlos.enrique.suca@gmail.com>
2026-09-22 21:46:39 +0100

    add:github pages workflow

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+const redirectedPath = sessionStorage.getItem('gh-pages-redirect')
+
+if (redirectedPath) {
+  sessionStorage.removeItem('gh-pages-redirect')
+  window.history.replaceState(null, '', redirectedPath)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
